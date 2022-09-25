35d61f85f8d18c04538d353c2d2561b5eff4a50d
Poshuk Vadym <vadimposhuk1999@gmail.com>
2022-09-25 14:38:34 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.header}>
+      <header className={styles.header_h1}>
+        <h1>hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
